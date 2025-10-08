7969945211938f194e5400ea1846dfa4890c18d2
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-08 08:54:17 -0400

    Update numbrguessgeam.py

--- numbrguessgeam.py
+++ numbrguessgeam.py
@@ -1,13 +1,22 @@
 import random
 
 number = random.randint(1, 10)
-
-guess = input("Guess first number between 1 and 10")
-
+correct = 0
 guesses = []
-while correct < 1:
+guess = int(input(f"Guess first number between 1 and 10: "))
+
+while correct == 0:
     if guess == number:
         correct = 1
         print(f"You guessed right you guessed{guesses}")
-    else:
+    elif guess < number:
+        print("It is greater")
         guesses.append(guess)
+        guess = int(input(f"Guess first number between 1 and 10: "))
+    elif guess > number:
+        print("It is less")
+        guesses.append(guess)
+        guess = int(input(f"Guess first number between 1 and 10: "))
+
+print(f"it is right, the answer is {number}")
+print (f"your guesses were {guesses}")
